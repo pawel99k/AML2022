266b05c7f9a4c3ecda7ebe3b3f04b3ed164fe297
pawel99k <pkozminski99@gmail.com>
2022-03-19 09:48:01 +0100

    gradient descent models

---
+++ Project1/measures.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def __is_classification_not_binary(y):
+    return ((y != 0) & (y != 1)).any()
+
+def __check_initial_conditions(y_test, y_pred):
+    if y_test.shape != y_pred.shape:
+        raise ValueError(f'Shapes of arguments are not the same. '
+                            f'y_test: {y_test.shape}, y_pred: {y_pred.shape}')
+    if __is_classification_not_binary(y_pred):
+        raise ValueError('y_pred contains non 0/1 values.')
+    if __is_classification_not_binary(y_test):
+        raise ValueError('y_test contains non 0/1 values.')
+
+def accuracy(y_pred, y_test, check_cond=True):
+    if check_cond:
+        __check_initial_conditions(y_test, y_pred)
+    return (y_pred == y_test).mean()
+
+def recall(y_pred, y_test, check_cond=True):
+    """
+    Recall = TPR = TP/(TP+FN)
+    """
+    if check_cond:
+        __check_initial_conditions(y_test, y_pred)
+    if not y_pred.sum():
+        raise ZeroDivisionError(f'Recall can not be calculated as there are no positive predictions.')
+    tp = ((y_pred==1) & (y_pred==y_test)).sum()
+    pv = y_test.sum()
+    return tp/pv
+
+def precision(y_pred, y_test, check_cond=True):
+    """
+    Precision = (TP)/(TP+FP)
+    """
+    if check_cond:
+        __check_initial_conditions(y_test, y_pred)
+    if not y_test.sum():
+        raise ZeroDivisionError(f'Recall can not be calculated as there are no positive observations.')
+    tp = ((y_pred==1) & (y_pred==y_test)).sum()
+    pp = y_pred.sum()
+    return tp/pp
+
+def f_measure(y_pred, y_test):
+    r = recall(y_pred, y_test)
+    p = precision(y_pred, y_test)
+    return 2*r*p/(r+p)
---
+++ Project1/optimizers.py
@@ -0,0 +1,103 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from statsmodels.tools import add_constant
+from scipy.special import expit
+from tqdm.notebook import tqdm
+
+class Optimizer(ABC):
+    
+    def __init__(self):
+        self.name = None
+    
+    @staticmethod
+    def do_early_stop(loss_list, check_no_progress_epochs=10):
+        if len(loss_list)<check_no_progress_epochs:
+            return False
+        last_loss_list = loss_list[-(check_no_progress_epochs+1):]
+        for l_i, loss in enumerate(last_loss_list):
+            if l_i+1>=check_no_progress_epochs:
+                return True
+            if loss > last_loss_list[l_i+1]:
+                return False
+    
+    @staticmethod
+    def binary_cross_entropy_loss(y_true, y_prob):
+        n = y_true.shape[0]
+        loss_value = -(np.log(y_prob[y_true]).sum() + np.log(1-y_prob[y_true==0]).sum())/n
+        return loss_value
+    
+    @abstractmethod
+    def check_arguments(self, kwargs) -> None:
+        pass
+    
+    @abstractmethod
+    def train(self, X, y) -> None:
+        pass
+    
+    @abstractmethod
+    def predict(self, X) -> np.ndarray:
+        pass
+        
+class GradientDescent(Optimizer):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.check_arguments(kwargs)
+        self.name = 'Gradient Descent'
+        self.w = None
+        self.losses = None
+        self.batch_size = kwargs['batch_size']
+        self.epochs = kwargs['epochs']
+        self.learning_rate = kwargs['learning_rate']
+        self.is_trained = False
+
+    @staticmethod
+    def gradients(X, y_true, y_pred):
+        n = len(X)
+        dw = X.T@(y_pred-y_true)/n
+        return dw
+    
+    @staticmethod
+    def check_arguments(kw):
+        required_arguments = {'batch_size', 'epochs', 'learning_rate'}
+        lacking_arguments = required_arguments - set(kw)
+        if len(lacking_arguments):
+            raise ValueError(f'Missing required arguments: {lacking_arguments}')
+        left_arguments = set(kw) - required_arguments
+        if len(left_arguments):
+            raise ValueError(f'Unused arguments: {left_arguments}')
+
+    def train(self, X, y):
+        X, y = X.copy(), y.copy()
+        X = add_constant(X)
+        n, k = X.shape
+        w = np.zeros((k, 1))
+        y = y.reshape((-1, 1))
+        losses = []
+        min_loss = np.inf
+        for e in tqdm(range(self.epochs)):
+            for i in range((n-1)//self.batch_size+1):
+                batch_begin = i*self.batch_size
+                batch_end = (i+1)*self.batch_size
+                x_batch_subset = X[batch_begin:batch_end, :]
+                y_batch_subset = y[batch_begin:batch_end]
+                y_pred_prob = expit(x_batch_subset @ w)
+                w_deriv = self.gradients(x_batch_subset, y_batch_subset, y_pred_prob)
+                w -= self.learning_rate * w_deriv
+            cur_loss = Optimizer.binary_cross_entropy_loss(y, expit(X @ w))
+            losses.append(cur_loss) 
+            if cur_loss < min_loss:
+                return_w = w
+            if Optimizer.do_early_stop(losses):
+                print('Early stopping')
+                break
+        self.w = return_w
+        self.losses = losses
+        self.is_trained = True
+    
+    def predict(self, X_new):
+        if not self.is_trained:
+            raise ValueError('This model has not been trained yet.')
+        X = X_new.copy()
+        X = add_constant(X)
+        return ((X @ self.w) >= 0).astype(int).reshape((-1,))
